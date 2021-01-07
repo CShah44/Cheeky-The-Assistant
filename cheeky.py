ee693037c8c57562acfe83ac6795ed226859cdce
CShah44 <chaityashah36@gmail.com>
2021-01-07 20:38:09 +0530

    Cheeky can listen

--- cheeky.py
+++ cheeky.py
@@ -1,13 +1,37 @@
 import pyttsx3
+import datetime
+import speech_recognition as sr
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 
 engine.setProperty('voice', voices[0].id)
 
+
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
+
+def TakeCommand():
+
+    r = sr.Recognizer()
+
+    with sr.Microphone() as src:
+        print('Listening')
+        r.pause_threshold = 1
+        audio = r.listen(src)
+    try:
+        print('Recognizing')
+        query = r.recognize_google(audio, language='en-in')
+        print(f'You said {query}\n')
+    except Exception as e:
+        print(e)
+        print('Unable to understand')
+        return None
+    return query
+
+
 if __name__ == '__main__':
     speak('Hello Sir')
+    TakeCommand()
